ab115183dcbc9ed986b300bb0e545a5534c04528
Yusuf_D <ydemirci7@gmail.com>
2022-02-04 20:20:06 +0300

    new features added

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+// Open the Modal
+function openModal() {
+  $("#myModal").css("display", "block");
+  
+}
+
+// Close the Modal
+function closeModal() {
+  $("#myModal").css("display", "none");
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = $(".mySlides");
+  const dots = $(".demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active selected", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active selected";
+}
+
 $(".increase").click(() => {
     $(".count").text() < 33 &&
     $(".count").text(+$(".count").text() + 1 + "")
@@ -8,10 +44,19 @@ $(".decrease").click(() => {
     $(".count").text(+$(".count").text() - 1 + "");
 })
 
-const images = $(".images__small")
+
 $(".images__small").click((e) => {
     $(".images__small").removeClass("selected")
     $(e.target).addClass("selected");
     
     $(".images__big").attr("src", (e.target.src).replace("-thumbnail", ""));
 })
+
+$(".demo").click((e) => {
+  $(".demo").removeClass("selected")
+  $(e.target).addClass("selected");
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
