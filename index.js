c9ebf2b7449422072d95cbfefe3cc6b5a2484c9d
Yusuf_D <ydemirci7@gmail.com>
2022-02-05 23:44:36 +0300

    index.js updated, shopping cart added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Open the Modal
 function openModal() {
   $("#myModal").css("display", "block");
-  
+  $("header").addClass("ui-state-disabled");
 }
 
 // Close the Modal
 function closeModal() {
   $("#myModal").css("display", "none");
+  $("header").removeClass("ui-state-disabled");
 }
 
 // Next/previous controls
@@ -22,39 +23,78 @@ function currentSlide(n) {
 function showSlides(n) {
   const slides = $(".mySlides");
   const dots = $(".demo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active selected", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active selected";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active selected";
+}
+
+function addToCart(count) {
+  $(".badge").text((+$(".badge").text() + count) + "")
+  let orderCount = $(".badge").text();
+
+  const img = $('<img />', {
+    class: 'cart-img', src: './images/image-product-1-thumbnail.jpg', alt: 'shoe'
+  });
+
+  const text = $(
+    `<div class='cart-text'> 
+      <p>Fall Limited Edition Sneakers</p>
+      <p>$125.00 x ${orderCount} <span>$${125*orderCount}.00</span></p>
+    </div>`)
+
+  const info = $(
+    `<div class='cart-info'> 
+      ${img[0].outerHTML}
+      ${text[0].outerHTML}
+      <i class="fas fa-trash-alt"></i>
+    </div>`)
+
+  const button = $("<button class='add'>Checkout</button>")
+
+  $('.cart-content').empty()
+  info.appendTo($('.cart-content'));
+  button.appendTo($('.cart-content'));
 }
 
 $(".increase").click(() => {
-    $(".count").text() < 33 &&
+  $(".count").text() < 33 &&
     $(".count").text(+$(".count").text() + 1 + "")
 })
 
 $(".decrease").click(() => {
-    $(".count").text() > 0 &&
+  $(".count").text() > 0 &&
     $(".count").text(+$(".count").text() - 1 + "");
 })
 
 
 $(".images__small").click((e) => {
-    $(".images__small").removeClass("selected")
-    $(e.target).addClass("selected");
-    
-    $(".images__big").attr("src", (e.target.src).replace("-thumbnail", ""));
+  $(".images__small").removeClass("selected")
+  $(e.target).addClass("selected");
+
+  $(".images__big").attr("src", (e.target.src).replace("-thumbnail", ""));
 })
 
 $(".demo").click((e) => {
   $(".demo").removeClass("selected")
   $(e.target).addClass("selected");
+})
+
+$(".add").click(() => {
+  let count = +$(".count").text();
+  $(".count").text("0")
+  addToCart(count)
+})
+
+$(".orders").click(() => {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 })
 
 let slideIndex = 1;
